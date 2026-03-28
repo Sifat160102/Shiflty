eccc7e1e8ae7201457c80b036157d023f33bfce0
seaiirr <seaiirr@mail.ru>
2026-03-29 01:53:07 +0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -25,7 +25,7 @@ export function Header() {
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
       <Link href="/" className="flex items-center gap-2">
   <img 
-    src="/ShiftlyLogo.png" 
+    src="public/ShiftlyLogo.png" 
     alt="Shiftly logo" 
     className="h-8 w-auto"
   />
